12dff92099849873b3665f5af1f2f8f849a6a0be
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-06-23 09:13:23 +0530

    updated test.py

--- test.py
+++ test.py
@@ -9,19 +9,19 @@ env = gym.make('dino_world/DinoWorld-v0')
 num_eps = 10
 for ep in range(num_eps):
     obs = env.reset()
+    print('Observation:', obs)
     env.render()
     done = False
 
     while not done:
         pygame.event.get()
         action = env.action_space.sample()  # Random action selection
-        
-        #action = 0
         print('Action:', action)    
         new_obs, reward, done, info= env.step(action)
         env.render()
+        print('New Observation:', new_obs)
         print('Reward:', reward)
         obs = new_obs
         pygame.time.wait(50)
-        
+
 env.close()
